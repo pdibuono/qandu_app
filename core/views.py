78d1295e81235e58fcc4b13e813ae2e32e533651
Phil <ptdibuono@quinnipiac.edu>
2015-10-01 19:15:43 +0000

    Add answers to question detail view

--- core/views.py
+++ core/views.py
@@ -12,17 +12,25 @@ class QuestionCreateView(CreateView):
     template_name = 'question/question_form.html'
     fields = ['title', 'description']
     success_url = reverse_lazy('question_list')
-    
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(QuestionCreateView, self).form_valid(form)
+
 class QuestionListView(ListView):
     model = Question
     template_name = 'question/question_list.html'
-    
+
 class QuestionDetailView(DetailView):
     model = Question
     template_name = 'question/question_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(QuestionDetailView, self).get_context_data(**kwargs)
+        question = Question.objects.get(id=self.kwargs['pk'])
+        answers = Answer.objects.filter(question=question)
+        context['answers'] = answers
+        return context
 
 class QuestionUpdateView(UpdateView):
     model = Question
@@ -33,15 +41,15 @@ class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'question/question_confirm_delete.html'
     success_url = reverse_lazy('question_list')
-    
+
 class AnswerCreateView(CreateView):
     model = Answer
     template_name = 'answer/answer_form.html'
     fields = ['text']
-    
+
     def get_success_url(self):
         return self.object.question.get_absolute_url()
-      
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         form.instance.question = Question.objects.get(id=self.kwargs['pk'])
