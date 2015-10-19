99a9f0e6511524b3567e37bad88de51923b260ac
Phil <ptdibuono@quinnipiac.edu>
2015-10-19 18:11:17 +0000

    Add user delete view

--- core/views.py
+++ core/views.py
@@ -141,20 +141,38 @@ class UserDetailView(DetailView):
         answers = Answer.objects.filter(user=user_in_view)
         context['answers'] = answers
         return context
-      
+
 class UserUpdateView(UpdateView):
     model = User
     slug_field = "username"
     template_name = "user/user_form.html"
     fields = ['email', 'first_name', 'last_name']
-    
+
     def get_success_url(self):
         return reverse('user_detail', args=[self.request.user.username])
-      
+
     def get_object(self, *args, **kwargs):
         object = super(UserUpdateView, self).get_object(*args, **kwargs)
         if object != self.request.user:
-          raise PermissionDenied()
+            raise PermissionDenied()
         return object
 
+class UserDeleteView(DeleteView):
+    model = User
+    slug_field = "username"
+    template_name = 'user/user_confirm_delete.html'
 
+    def get_success_url(self):
+        return reverse_lazy('logout')
+
+    def get_object(self, *args, **kwargs):
+        object = super(UserDeleteView, self).get_object(*args, **kwargs)
+        if object != self.request.user:
+            raise PermissionDenied()
+        return object
+
+    def delete(self, request, *args, **kwargs):
+        user = super(UserDeleteView, self).get_object(*args)
+        user.is_active = False
+        user.save()
+        return redirect(self.get_success_url())
